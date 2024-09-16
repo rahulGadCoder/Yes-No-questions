5b22d4955982ea9433aedb8079442a4b50b59130
Rahul Gadling <56485849+rahulGadCoder@users.noreply.github.com>
2024-09-16 21:17:03 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+import React, { Component } from "react";
+import QuizComponent from "./QuizComponent";
+
+class App extends Component {
+  state = {
+  };
+
+
+  render() {
+    return (
+      <div>
+        <QuizComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
 
